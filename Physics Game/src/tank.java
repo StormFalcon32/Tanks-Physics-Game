7323825e1c4c1d941b8d15ac7e90b3e82f45e52d
StormFalcon32 <benchen9708@gmail.com>
2019-05-18 17:09:21 -0400

    Split does damage now

--- Physics Game/src/tank.java
+++ Physics Game/src/tank.java
@@ -12,9 +12,8 @@ public class tank extends object {
 	
 	double velocity = 0;
 	// launch velocity, default 75
-	
-	String type = "Split";
-	// attack type, default Bullet
+	int type = 0;
+	// attack type
 	
 	boolean up;
 	boolean down;
@@ -86,15 +85,19 @@ public class tank extends object {
 			return;
 		// if low on ammo, don't shoot
 		attack a = null;
-		if (type.equals("Bullet")) {
+		if (type == 0) {
 			a = new attack(bx, y, angle, velocity, 20, c);
 			// offset shot so it starts in the middle of the tank
-		} else if (type.equals("Split")) {
-			a = new splitbomb(bx, y, angle, velocity, 20, c);
+		} else if (type == 1) {
+			a = new splitbomb(bx, y, angle, velocity, 180, c);
 		}
 		attacks.add(a);
 		ammo -= 10;
 		// adds an attack, lowers ammo
+	}
+	
+	public void switchW() {
+		type = (type + 1) % 2;
 	}
 	
 	public void genTrajectory() {
